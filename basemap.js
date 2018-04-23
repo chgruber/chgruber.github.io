e6cedf735d91f2e1a371d8e4e0c01a9246c397e7
Gruber <csap3389@uibk.ac.at>
2018-04-23 15:39:45 +0200

    aenderung bis zur letzten stunde

---
+++ basemap.js
@@ -0,0 +1,82 @@
+let myMap = L.map("mapdiv");
+let myLayers = {
+    osm : L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>OpenStreetMap"
+        }
+    ),
+    //* {s} steht sämtliche subdomain - maps,1,2,3,4
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    // attribution = Datenquelle (Vorgegeben wie man es angeben muss )
+    ),
+    bmapoverlay :  L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmapgrau : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaphidpi : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaporthofoto30cm : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+}
+
+myMap.addLayer(myLayers.geolandbasemap);
+
+let myMapControl = L.control.layers({
+    "OpenStreetMap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,
+   
+    "basemap.at grau" : myLayers.bmapgrau,
+    "basemap.at highdpi" : myLayers.bmaphidpi,
+    "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
+},{
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+});
+myMap.addControl(myMapControl);
+
+// Einstellungen 11 = Zoomfaktor
+
+myMap.setView([47.267,11.383],11);
+
+/* Objekte erstellen
+let myLayers = {
+    wert : 100,
+    alter : 50,
+    farbe : "grün",
+    liste : [1,2,3,4],
+    nocheinobjekt = {
+
+    }
+}
+*/
+
+
+/*
+myLayer = L.tileLayer("https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png")
+// let url = "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
+
+myLayer = L.tileLayer("https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png")
+myLayer = L.tileLayer("https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png")
+myLayer = L.tileLayer("https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg")
+myLayer = L.tileLayer("https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg")
+*/
+
