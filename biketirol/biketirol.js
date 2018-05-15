934b10917028a60b91023c458061c7c1ee3fc323
chgruber <37377813+chgruber@users.noreply.github.com>
2018-05-16 00:33:53 +0200

    Karten und Route funktionieren

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -13,7 +13,7 @@
 let myMap = L.map("map");
 // eine neue Leaflet Karte definieren
 
-const etappe09group = L.featureGroup();
+const etappe09Group = L.featureGroup();
 
 // Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
 // WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
@@ -31,12 +31,7 @@ let myLayers = {
         attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>OpenStreetMap"
         }
     ), 
-    bmapoverlay :  L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
-        }
-    ),
+   
     bmaporthofoto30cm : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
         subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
@@ -64,38 +59,57 @@ let myLayers = {
         pane: "overlayPane"                         
         }
     ),  
-}; 
-myMap.addLayer(myLayers.geolandbasemap)
+}
+myMap.addLayer(myLayers.geolandbasemap); 
 // Maßstab metrisch ohne inch
-
-L.control.scale({      
-    maxWidth: 200,      
-    metric: true,       
-    imperial: false     
+//Maßstabsleiste
+L.control.scale({       // http://leafletjs.com/reference-1.3.0.html#control-scale
+    maxWidth: 200,      // http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
+    metric: true,       // http://leafletjs.com/reference-1.3.0.html#control-scale-metric
+    imperial: false     // http://leafletjs.com/reference-1.3.0.html#control-scale-imperial
 }).addTo(myMap);
 
+
 // Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
+
 const marker_Kaiserhaus = [47.536631,11.912908]
 let Kaiserhaus_popup = '<h1>Kaiserhaus </h1>https://de.wikipedia.org/wiki/Kaiserklamm<a href="">Wikipedia</a>'
 L.marker(marker_Kaiserhaus, {icon: L.icon({
   iconUrl: '../icons/bike.png',
 
 })
-}).addTo(etappe09group).bindPopup(Kaiserhaus_popup);
+}).addTo(etappe09Group).bindPopup(Kaiserhaus_popup);
 
 const marker_Kufstein = [47.605964,12.196378]
 let Kufstein_popup = '<h1>Kufstein </h1>https://de.wikipedia.org/wiki/Kufstein<a href="">Wikipedia</a>'
 L.marker(marker_Kufstein, {icon: L.icon({
-  iconUrl: '../icons/bike.png',
+ iconUrl: '../icons/bike.png',
 })
 }).addTo(etappe09Group).bindPopup(Kufstein_popup);
 
 
 // GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-
 // Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
-let geojson = L.geoJSON(etappe09).addTo(etappe09group);
+
+let geojson = L.geoJSON(etappe09).addTo(etappe09Group);
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
+let myMapControl  = L.control.layers({                
+    "Geoland Basemap" : myLayers.geolandbasemap,
+    "OpenStreetMap" : myLayers.osm,
+    "bma_Orthofoto" : myLayers.bmaporthofoto30cm,
+    "gdi_Orthofoto" : myLayers.gdi_ortho,
+    "gdi_Sommer"    : myLayers.gdi_sommer,
+    "gdi_Winter"    : myLayers.gdi_winter,
+    
+  }, {
 
 // Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+"gdi_Nomenklatur" : myLayers.gdi_nomenklatur,
+} ); 
+
+myMap.addControl(myMapControl);
+
+myMap.addLayer(etappe09Group)
+
+myMap.fitBounds(etappe09Group.getBounds());
